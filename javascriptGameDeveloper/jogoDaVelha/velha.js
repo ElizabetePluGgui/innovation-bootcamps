de39cb29049327b9b23687f12430cd37113f3957
Elizabete Fabri <elizabete958@gmail.com>
2021-02-04 00:40:02 -0300

    ok

--- javascriptGameDeveloper/jogoDaVelha/velha.js
+++ javascriptGameDeveloper/jogoDaVelha/velha.js
@@ -1,3 +1,17 @@
-function escolherQuadrado() {
-  console.log('clicou no botao');
+var jogador = null;
+var jogadorSelecionado = document.getElementById({
+  elementId: 'jogador-selecionado',
+});
+
+mudarJogador({ valor: 'X' });
+
+function escolherQuadrado(id) {
+  var quadrado = document.getElementById(id);
+
+  quadrado.innerHTML = jogador;
+  quadrado.style.color = '#000';
+}
+function mudarJogador(valor) {
+  jogador = valor;
+  jogadorSelecionado.innerHTML = jogador;
 }
