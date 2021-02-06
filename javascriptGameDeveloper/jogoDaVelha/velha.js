52757918115b993a0fe7cbccdd43ffe3351f16f4
Elizabete Fabri <elizabete958@gmail.com>
2021-02-06 13:19:34 -0300

    ok

--- javascriptGameDeveloper/jogoDaVelha/velha.js
+++ javascriptGameDeveloper/jogoDaVelha/velha.js
@@ -88,3 +88,42 @@ function checaVencedor() {
     mudarVencedor(quadrado3);
   }
 }
+
+function mudarVencedor(quadrado) {
+  vencedor = quadrado.innerHTML;
+  vencedorSelecionado.innerHTML = vencedor;
+}
+
+function mudaCorQuadrado(quadrado1, quadrado2, quadrado3) {
+  quadrado1.style.background = '#0f0';
+  quadrado2.style.background = '#0f0';
+  quadrado3.style.background = '#0f0';
+}
+
+function checaSequencia(quadrado1, quadrado2, quadrado3) {
+  var eigual = false;
+
+  if (
+    quadrado1.innerHTML !== '-' &&
+    quadrado1.innerHTML === quadrado2.innerHTML &&
+    quadrado2.innerHTML === quadrado3.innerHTML
+  ) {
+    eigual = true;
+  }
+
+  return eigual;
+}
+
+function reiniciar() {
+  vencedor = null;
+  vencedorSelecionado.innerHTML = '';
+
+  for (var i = 1; i <= 9; i++) {
+    var quadrado = document.getElementById(i);
+    quadrado.style.background = '#eee';
+    quadrado.style.color = '#eee';
+    quadrado.innerHTML = '-';
+  }
+
+  mudarJogador('X');
+}
