78294de4f79d7db3315b414ec19c0f32bbd970cb
Elizabete Fabri <elizabete958@gmail.com>
2021-02-06 13:17:17 -0300

    ok

--- javascriptGameDeveloper/jogoDaVelha/velha.js
+++ javascriptGameDeveloper/jogoDaVelha/velha.js
@@ -5,7 +5,13 @@ var vencedorSelecionado = document.getElementById('vencedor-selecionado');
 mudarJogador('X');
 
 function escolherQuadrado(id) {
+  if (vencedor !== null) {
+    return;
+  }
   var quadrado = document.getElementById(id);
+  if (quadrado.innerHTML !== '-') {
+    return;
+  }
 
   quadrado.innerHTML = jogador;
   quadrado.style.color = '#000';
@@ -16,8 +22,69 @@ function escolherQuadrado(id) {
     jogador = 'X';
   }
   mudarJogador(jogador);
+  checaVencedor();
 }
+
 function mudarJogador(valor) {
   jogador = valor;
   jogadorSelecionado.innerHTML = jogador;
+}
+
+function checaVencedor() {
+  var quadrado1 = document.getElementById(1);
+  var quadrado2 = document.getElementById(2);
+  var quadrado3 = document.getElementById(3);
+  var quadrado4 = document.getElementById(4);
+  var quadrado5 = document.getElementById(5);
+  var quadrado6 = document.getElementById(6);
+  var quadrado7 = document.getElementById(7);
+  var quadrado8 = document.getElementById(8);
+  var quadrado9 = document.getElementById(9);
+
+  if (checaSequencia(quadrado1, quadrado2, quadrado3)) {
+    mudaCorQuadrado(quadrado1, quadrado2, quadrado3);
+    mudarVencedor(quadrado1);
+    return;
+  }
+
+  if (checaSequencia(quadrado4, quadrado5, quadrado6)) {
+    mudaCorQuadrado(quadrado4, quadrado5, quadrado6);
+    mudarVencedor(quadrado4);
+    return;
+  }
+
+  if (checaSequencia(quadrado7, quadrado8, quadrado9)) {
+    mudaCorQuadrado(quadrado7, quadrado8, quadrado9);
+    mudarVencedor(quadrado7);
+    return;
+  }
+
+  if (checaSequencia(quadrado1, quadrado4, quadrado7)) {
+    mudaCorQuadrado(quadrado1, quadrado4, quadrado7);
+    mudarVencedor(quadrado1);
+    return;
+  }
+
+  if (checaSequencia(quadrado2, quadrado5, quadrado8)) {
+    mudaCorQuadrado(quadrado2, quadrado5, quadrado8);
+    mudarVencedor(quadrado2);
+    return;
+  }
+
+  if (checaSequencia(quadrado3, quadrado6, quadrado9)) {
+    mudaCorQuadrado(quadrado3, quadrado6, quadrado9);
+    mudarVencedor(quadrado3);
+    return;
+  }
+
+  if (checaSequencia(quadrado1, quadrado5, quadrado9)) {
+    mudaCorQuadrado(quadrado1, quadrado5, quadrado9);
+    mudarVencedor(quadrado1);
+    return;
+  }
+
+  if (checaSequencia(quadrado3, quadrado5, quadrado7)) {
+    mudaCorQuadrado(quadrado3, quadrado5, quadrado7);
+    mudarVencedor(quadrado3);
+  }
 }
